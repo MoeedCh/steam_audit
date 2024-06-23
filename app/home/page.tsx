b1f0ec92cf4af3b01c8be0ff711d3f54d6497e83
MoeedCh <moeedchaudhry15@gmail.com>
2024-06-23 17:16:14 -0400

    added landing page post auth

---
+++ app/home/page.tsx
@@ -0,0 +1,15 @@
+'use client'; // I dont fuck with SSR its confusing and i hate it
+
+import Image from "next/image";
+import styles from "../../styles/Home.module.css"
+import { Button } from "@mui/material";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      
+      MY BRUDDA U LIT AF U MADE IT IN TURN ME TF UP HAHAAHAH XD
+
+    </div>
+  );
+}
